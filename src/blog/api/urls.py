4eeea4f1935ab3d676a605eaecec90c7e5c10dc8
Noyon <noyonict@gmail.com>
2018-09-09 18:09:40 +0600

    Comment url

--- src/blog/api/urls.py
+++ src/blog/api/urls.py
@@ -7,11 +7,18 @@ router = routers.DefaultRouter()
 router.register(r'', PostViewSet)
 
 urlpatterns = [
+    # Django function base Api View
     path('post-list/', all_post, name='all_post'),
     path('post-list/<int:id>/', single_post, name='single_post'),
+
+    # Django Generic Model Mixin Api View
     path('post-generic-mixin-view/', PostGenericMixinAPIView.as_view()),
     path('post-generic-mixin-view/<int:id>/', PostGenericMixinAPIView.as_view()),
+
+    # Django Class base Api View
     path('post-class-view/', BlogAIPView.as_view()),
     path('post-class-view/<int:id>/', BlogDetailAPIView.as_view()),
+
+    # Django class base and router urls view
     path('post/', include(router.urls)),
 ]
--- src/demo1/api/urls.py
+++ src/demo1/api/urls.py
@@ -8,12 +8,19 @@ router = routers.DefaultRouter()
 router.register('', EmployeeViewSets)
 
 urlpatterns = [
+    # Generic model Mixin Api View
     path('generic-mixin-api-view/', EmployeeModelMixinAPIView.as_view()),
     path('generic-mixin-api-view/<int:id>/', EmployeeModelMixinAPIView.as_view()),
+
+    # Django class base Api view
     path('employees-class-view/', DemoAPIView.as_view()),
     path('employees-class-view/<int:id>/', DemoDetailAPIView.as_view()),
+
+    # Django function base Api View
     path('employees/', all_employees, name='all_employees'),
     path('employees/<int:id>/', employee_detail, name='employee_detail'),
+
+    # Django class base and router api view
     path('', include(router.urls)),
 ]
 
